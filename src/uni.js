a9aec4f3a6410739f4db711b835908c7e90a90b4
Archan Patkar <pat@archanpatkar.com>
2021-05-03 22:04:17 +0530

    Update

--- src/uni.js
+++ src/uni.js
@@ -37,18 +37,17 @@ function unify(f1,f2,subst={}) {
     if(atomic(f1) && atomic(f2)) return f1 === f2?subst:failed;
     if(Var.is(f1)) return unifyvar(f1,f2,subst);
     if(Var.is(f2)) return unifyvar(f2,f1,subst);
-    if(Array.isArray(f1) && Array.isArray(f2) && f1.length === f2.length) {
-        let prev = {}
-        for(let i = 0; i < f1.length;i++) {
-            prev = unify(f1[i],f2[i],prev);
-        }
-        return prev;
+    if(Array.isArray(f1) && Array.isArray(f2)) {
+        const k1 = Object.keys(f1).map(i => parseInt(i));
+        const k2 = Object.keys(f2).map(i => parseInt(i));
+        const common = Array.from(new Set(Array.from([...k1,...k2]).filter(k => k1.includes(k) && k2.includes(k))));
+        return common.reduce((prev,key) => unify(f1[key],f2[key],prev),{});
     }
     if(typeof f1 === "object" && typeof f2 === "object") {
         const k1 = Object.keys(f1);
         const k2 = Object.keys(f2);
-        const common = Array.from([...k1,...k2]).filter(k => k1.includes(k) && k2.includes(k));
-        return common.reduce((prev,key) => unify(f1[key],f2[key],prev),{})   
+        const common = Array.from(new Set(Array.from([...k1,...k2]).filter(k => k1.includes(k) && k2.includes(k))));
+        return common.reduce((prev,key) => unify(f1[key],f2[key],prev),{});  
     }
     return failed;
 }
@@ -56,7 +55,6 @@ function unify(f1,f2,subst={}) {
 function unify_all(eqs) {
     return eqs.reduce((acc,v) => unify(v[0],v[1],acc),{});
 }
-
 
 const u1 = unify(["g",Var("x2")],Var("x1"));
 const u2 = unify(["f",Var("x1"),["h",Var("x1")],Var("x2")],["f",["g",Var("x3")],Var("x4"),Var("x3")]);
